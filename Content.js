25ed245732d7450f589d2f11d996889a09e77f19
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-08-18 01:43:01 +0530

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,5 +1,5 @@
 var url = window.location.href;
-if (url.includes("http://172.16.222.1:1000/login?")) {
+if (url.includes("http://172.16.222.1:1000/")) {
   function fillforMe() {
     document.querySelector("#ft_un").value = "";//enter your rollnumber
     document.querySelector("#ft_pd").value = "";//enter your password
