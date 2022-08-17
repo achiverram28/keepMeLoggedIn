1d6aa2332422621028022ebb98d5bb54b4284c23
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-08-17 16:45:21 +0530

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,8 +1,8 @@
 var url = window.location.href;
 if (url.includes("http://172.16.222.1:1000/login?")) {
   function fillforMe() {
-    document.querySelector("#ft_un").value = "2021BCS0024";
-    document.querySelector("#ft_pd").value = "DT78UJ56";
+    document.querySelector("#ft_un").value = "";//enter your rollnumber
+    document.querySelector("#ft_pd").value = "";//enter your password
     document.querySelector('input[value="Continue"]').click();
   }
   function startit() {
